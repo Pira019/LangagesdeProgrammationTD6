ac91efdf92af947f5ec043bdc4c99967f90fa75b
pires lufungula <pires00243@gmail.com>
2024-03-20 04:38:29 -0400

    result

--- main.py
+++ main.py
@@ -26,16 +26,32 @@ def memo(f):
 		return cache[t]
 	return fcache
 
+# Affiche les termes de la suite, et indique le temps écoulé
+def enumerer(suite):
+  t = time.time()
+  count = 0
+  for term in suite:
+    dt = (time.time() - t)*1000
+    print (count,":", term, "({temps:.2f} ms)".format(temps=dt))	
+    count +=1
 
 #fonction qui calcule le nième terme de la suite de Fibonacci
-@trace 
+#@trace 
 @memo
 def fib(n):
     if (n == 0 or n == 1):
         return n
     return fib(n - 1) + fib(n - 2)
 
-fib(8)
+#fib(8)
 #memo(trace(fib(2)))
 
-
+#fonction partie 1
+def partie1():
+  #calculer les 20 premiers termes de la suite de Fibonacci
+  #1.1
+  suite = [fib(i) for i in range(20)]
+  #afficher les valeurs calculées
+  enumerer(suite)
+##Code exécuté --------
+partie1()
