54d9732184811d410934e35d3b3609fb4ec8f0c3
pires lufungula <pires00243@gmail.com>
2024-03-20 03:45:27 -0400

    2.1

--- main.py
+++ main.py
@@ -8,19 +8,8 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 
-#générateur infini 1.4
-def fibGen(): #génère la suite de fibonacci au complet (générateur infini...)
-    i=0
-    while(True):             
-        yield fib(i)
-        i +=1
+def partie2():
+  print("fib(4)", fib(5))
 
-#enumération limitée à 10 ms (Q1.5)
-def enumerer2(suite):
-	count = 0
-	for term in suite:
-		count +=1
-		dt = (time.time() - t)*1000
-		print (count, term, "({temps:.2f} ms)".format(temps=dt))	
-		if (dt>10):
-			break        
+  ##Code exécuté --------
+partie2()
